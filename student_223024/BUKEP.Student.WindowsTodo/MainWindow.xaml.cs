da1c72be27d4d7cc4416a491f5935b8676538685
RodionGromo <rodion.gromo@gmail.com>
2023-11-25 13:59:00 +0300

    ох ужи эти случайные директивы using

--- student_223024/BUKEP.Student.WindowsTodo/MainWindow.xaml.cs
+++ student_223024/BUKEP.Student.WindowsTodo/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace BUKEP.Student.WindowsTodo
--- student_223024/BUKEP.Student.WindowsTodo/RemoveModalWindow.xaml.cs
+++ student_223024/BUKEP.Student.WindowsTodo/RemoveModalWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace BUKEP.Student.WindowsTodo
 {
