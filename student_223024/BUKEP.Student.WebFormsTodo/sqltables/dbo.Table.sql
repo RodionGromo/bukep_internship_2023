d6fefb97f2d9108a91cfcdbcdd902358f119649b
RodionGromo <rodion.gromo@gmail.com>
2023-12-27 20:29:48 +0300

    лишнее

--- student_223024/BUKEP.Student.WebFormsTodo/sqltables/dbo.Table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] TEXT NOT NULL, 
-    [Description] TEXT NULL
-)
--- student_223024/BUKEP.Student.WebFormsTodo/sqltables/dbo.TaskTable.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].TaskTable
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] TEXT NOT NULL, 
-    [Description] TEXT NULL
-)
